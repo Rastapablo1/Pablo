ecb76ac6f9711e74f8ae1b7daa39927e9e6c1f5b
Pablo Salazar <pablosalazar66@hotmail.com>
2020-11-06 23:21:12 -0500

    Se sube el archivo completo

---
+++ Loginsemana3/Ejercicio1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Loginsemana3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ejercicio1 : ContentPage
+    {
+        public Ejercicio1()
+        {
+            InitializeComponent();
+        }
+
+        private void Sumar_Clicked(object sender, EventArgs e)
+        {
+            try {
+                double dato1 = Convert.ToDouble(txtDato1.Text);
+                double dato2 = Convert.ToDouble(txtDato2.Text);
+                double suma = dato1 + dato2;
+
+                txtResultado.Text = Sumar.ToString(); 
+            }
+            catch (Exception ex) {
+                DisplayAlert("Mensaje", "ERROR" + ex.Message, "OK");
+            }
+    }
+}
---
+++ Loginsemana3/PantallaInicio.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Loginsemana3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PantallaInicio : ContentPage
+    {
+        public PantallaInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludo_Clicked(object sender, EventArgs e)
+        {
+
+            string Text = txtNombre.Text;
+            string mensaje = "Hola " + Text;
+            DisplayAlert("Saludos", mensaje, "OK");
+        }
+    }
+}
